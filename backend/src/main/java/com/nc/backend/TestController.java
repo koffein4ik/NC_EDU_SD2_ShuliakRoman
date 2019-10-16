83273fb5594f9a35b773fe8994ebca042b5027ed
koffein4ik <koffein4ik@gmail.com>
2019-10-17 01:50:19 +0300

    Initial project version

---
+++ backend/src/main/java/com/nc/backend/TestController.java
@@ -0,0 +1,13 @@
+package com.nc.backend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
