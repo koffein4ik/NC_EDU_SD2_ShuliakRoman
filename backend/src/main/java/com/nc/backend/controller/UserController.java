567a81612974e8c5a16b08a11a0e26dde0761a34
koffein4ik <koffein4ik@gmail.com>
2019-11-02 13:28:58 +0300

    Registration added to user controller

--- backend/src/main/java/com/nc/backend/controller/UserController.java
+++ backend/src/main/java/com/nc/backend/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.nc.backend.controller;
 
-import com.nc.backend.model.User;
-import com.nc.backend.repositories.UserRepository;
+import com.nc.backend.model.UserEntity;
 import com.nc.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,16 +14,30 @@ public class UserController {
     UserService userService;
 
     @GetMapping("show")
-    Iterable<User> getAllUsers() {
+    Iterable<UserEntity> getAllUsers() {
         return userService.findAll();
     }
 
     @RequestMapping(path="/update", method= RequestMethod.GET)
     public String updateUser(@RequestParam int id, @RequestParam String newDescription) {
-        Optional<User> user = userService.findById(id);
-        User user1 = user.get();
-        user1.setProfile_description(newDescription);
+        Optional<UserEntity> user = userService.findById(id);
+        UserEntity user1 = user.get();
+        user1.setProfileDescription(newDescription);
         userService.save(user1);
         return "Updated successfully";
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(value = "/regnewuser", consumes = "application/json", produces = "application/json")
+    public UserEntity regNewUser(@RequestBody UserEntity userEntity) {
+        userService.regNewUser(userEntity);
+        return userEntity;
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value = "/nickname/{nickname}", method = RequestMethod.GET)
+    public UserEntity getUserByNickname(@PathVariable(name = "nickname") String nickname) {
+        Optional<UserEntity> userEntity = userService.getUserByNickname(nickname);
+        return userEntity.get();
     }
 }
