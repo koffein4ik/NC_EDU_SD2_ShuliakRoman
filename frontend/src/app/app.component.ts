edf7a2d42c66dbda34a515482ce3f88deb1be6fd
koffein4ik <koffein4ik@gmail.com>
2019-11-02 13:12:17 +0300

    Registration added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+
+  // constructor(private hhtp: HttpClient) { }
+  
+  // ngOnInit(){
+  //   console.log("init");
+  //   this.hhtp.get('http://localhost:8081/getusers').subscribe((value) => console.log(value));
+  // }
+
+  constructor() { }
+
+  ngOnInit() {
+    //this.getUsersService.get('http://localhost:8081/getusers').subscribe((value) =>
+    //  console.log(value));
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { IgxDatePickerModule} from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserpageComponent } from './components/userpage/userpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    RegistrationComponent,
+    UserpageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    IgxDatePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
