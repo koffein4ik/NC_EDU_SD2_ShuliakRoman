afd19611b1efb9522703ebf1de43cfa0702ce181
koffein4ik <koffein4ik@gmail.com>
2019-11-02 13:23:23 +0300

    User repository upd

--- backend/src/main/java/com/nc/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/nc/backend/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package com.nc.backend.repositories;
 
-import com.nc.backend.model.User;
-import org.springframework.data.repository.CrudRepository;
+import com.nc.backend.model.UserEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Integer> {
+    UserEntity findByNickname(String nickname);
+    //String regNewUser(UserEntity userEntity);
 }
