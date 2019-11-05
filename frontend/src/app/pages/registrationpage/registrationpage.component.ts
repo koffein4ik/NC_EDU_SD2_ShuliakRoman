0a9c98005a9bf0e8fb4430afb7ca1ad044ecf850
koffein4ik <koffein4ik@gmail.com>
2019-11-05 14:24:53 +0300

    Registration is working

---
+++ frontend/src/app/pages/registrationpage/registrationpage.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { RegistrationComponent} from './components/registration/registration.component';
+import { NavbarComponent} from  '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-registrationpage',
+  templateUrl: './registrationpage.component.html',
+  styleUrls: ['./registrationpage.component.css']
+})
+export class RegistrationpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
